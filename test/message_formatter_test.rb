230abc0f2ecf1594a00c31554ee2b25d0c5802b5
Olivia Charles <ocharles92@gmail.com>
2021-03-08 15:00:13 -0500

    Refactored tests and added more of them

--- test/message_formatter_test.rb
+++ test/message_formatter_test.rb
@@ -8,13 +8,21 @@ class MessageFormatterTest < Minitest::Test
   def test_it_can_split_lines
     message_format = MessageFormatter.new("I like cats")
     assert_equal ["I like cats"], message_format.split_lines
+  end
 
-    message_format1= MessageFormatter.new("I like cats and they are all so cute and amazing with small noses")
+  def test_it_can_split_lines_with_over_40_characters
+    message_format= MessageFormatter.new("I like cats and they are all so cute and amazing with small noses")
+
     expected = ["I like cats and they are all so cute and", " amazing with small noses"]
-    assert_equal expected, message_format1.split_lines
+    
+    assert_equal expected, message_format.split_lines
+  end
 
+    def test_it_can_test_split_lines_with_over_80_characters
     message_format2 = MessageFormatter.new("I like cats and they are all so cute and amazing with small noses cute little deadly claws and tails that are so swishy")
+
     expected = ["I like cats and they are all so cute and", " amazing with small noses cute little de", "adly claws and tails that are so swishy"]
+
     assert_equal expected, message_format2.split_lines
   end
 end
