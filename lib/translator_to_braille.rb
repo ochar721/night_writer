5f88bbe4b84f65518d17f1363ff91fc4068e5984
Olivia Charles <ocharles92@gmail.com>
2021-03-08 15:35:28 -0500

    Fixed merge conflict

--- lib/translator_to_braille.rb
+++ lib/translator_to_braille.rb
@@ -5,14 +5,6 @@ class TranslateToBraille
   include BrailleAlphabet
 
   def initialize(split_messages)
-<<<<<<< HEAD
-    @split_messages = split_messages
-    @braille = ""
-  end
-
-  def translate_message
-    @split_messages.each do |split_message|
-=======
     @split_messages     = split_messages
     @translated_message = ""
   end
@@ -26,7 +18,6 @@ class TranslateToBraille
   end
 
   def braille_creater(letter)
->>>>>>> 910d09cc661ea580c9a3dee2b4716490f21be009
     3.times do |index|
       letter.chars.each do |character|
         @translated_message += alphabet[character][index]
